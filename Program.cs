31caa5a99edfcece0a24d1d283a3bf026b0be4f4
Ralph L <134184625+ralphl22@users.noreply.github.com>
2024-05-15 17:50:17 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ double monsterArmorPoint = 15;
 // Shows what the starting HP and AP are for the monster
 
 
-    Console.WriteLine($"A monster appears with {monsterHealthPoint} HP and {monsterArmorPoint} AP!");
-    Console.WriteLine("For how much HP do you want to damage the monster?"); // Input any number you want, including decimals
+Console.WriteLine($"A monster appears with {monsterHealthPoint} HP and {monsterArmorPoint} AP!");
+Console.WriteLine("For how much HP do you want to damage the monster?"); // Input any number you want, including decimals
 
 
 // Converts any number that you input from a string into a double so that it can also accept decimal numbers
@@ -30,7 +30,7 @@ double currentMonsterArmorPoint = monsterArmorPoint - damagePointNumber;
 Console.WriteLine($"You strike the monster with your sword. It has {currentMonsterHealthPoint} HP and {currentMonsterArmorPoint} AP!");
 Console.Beep(400, 1000);
 
-// If the monster's HP and AP are both 0 then it is dead
+// If the monster's HP and AP are both <=0 then it is dead
 
 if (currentMonsterHealthPoint <= 0 && currentMonsterArmorPoint <= 0)
     Console.WriteLine("The monster is dead! Good job!");
